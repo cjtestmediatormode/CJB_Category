95ba32067fbf8d65160d99c2e21d4fbd26cfe367
zhusf <496795352@qq.com>
2019-02-26 10:25:28 +0800

    init

---
+++ CJB_Category/Classes/CTMediator+CJB.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+CJB.h
+//  CJB_Category_Example
+//
+//  Created by zhusf on 2019/2/26.
+//  Copyright © 2019 496795352@qq.com. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (CJB)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CJB_Category/Classes/CTMediator+CJB.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+CJB.m
+//  CJB_Category_Example
+//
+//  Created by zhusf on 2019/2/26.
+//  Copyright © 2019 496795352@qq.com. All rights reserved.
+//
+
+#import "CTMediator+CJB.h"
+
+@implementation CTMediator (CJB)
+
+@end
